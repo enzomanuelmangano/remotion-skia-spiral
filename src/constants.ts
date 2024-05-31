94536fabd0c3b06da1a9739f5857c390cae9ed4e
Enzo Manuel Mangano <enzomanuelmangano@gmail.com>
2024-05-31 15:15:37 +0200

    docs: constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,16 +1,19 @@
-// Const initialAngle = (Math.PI / 2) * 0.8306732868799129; // Math.random()
+// I just played with random values until I found something that looked 🤌
+// I collected the values that I liked the most and put them here :)
+
+// const initialAngle = (Math.PI / 2) * 0.8306732868799129; // Math.random()
 // const finalAngle = Number(Math.PI * 0.7555763789428693); // Math.random()
 
 // const initialAngle = 5.318141825150567;
 // const finalAngle = 3.145421591822945;
 
-// Const initialAngle = 4.26075108160635;
+// const initialAngle = 4.26075108160635;
 // const finalAngle = 3.8876065819216645;
+
+// const initialAngle = Math.random() * Math.PI * 2;
+// const finalAngle = Math.random() * Math.PI * 2;
 
 export const initialAngle = 6.033189907995227;
 export const finalAngle = 6.280537712961424;
-
-// Const initialAngle = Math.random() * Math.PI * 2;
-// const finalAngle = Math.random() * Math.PI * 2;
 
 export const MAX_DISTANCE_FROM_CENTER = 10000;
